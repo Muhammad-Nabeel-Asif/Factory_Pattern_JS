8f91d41e85c5c8ba8f0e4df6b9ea0f021634fa73
Nabeel Asif <nabeel.asif362@gmail.com>
2021-06-01 16:28:28 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const changeToHex = function (r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+const changeToRgb = function (r, g, b) {
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const makeColor = function (r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+  color.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+  return color;
+};
+
+const firstColor = makeColor(150, 250, 69);
