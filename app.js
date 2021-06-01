0f2c7bdb98af1163fb7f02de61166cf24bf5d578
Nabeel Asif <nabeel.asif362@gmail.com>
2021-06-01 17:15:53 +0500

    include Constructor Functions

--- app.js
+++ app.js
@@ -28,3 +28,34 @@ const makeColor = function (r, g, b) {
 };
 const firstColor = makeColor(150, 250, 69);
 //@@
+
+// $$
+// Introducing our constructor functions :
+/*
+The "new" keyword does the following things:
+1.Creates a blank, plain JavaScript object.
+2.Adds a property to the new object (__proto__) that links to the constructor function's prototype object.
+3.Binds the newly created object instance as the this context (i.e. all references to this in the constructor function now refer to the object created in the first step).
+4.Returns this if the function doesn't return an object.
+*/
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+const color1 = new Color(40, 60, 80);
+color1.hex();
+const color2 = new Color(250, 150, 99);
+color2.hex();
